512586945db8c37c8177604aafb60da19d314da0
wuzaichang <wuzaichang@wuzaichangdembp.cn.ibm.com>
2019-12-21 22:53:24 +0800

    Add category create UI and logic

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -1,22 +1,26 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Main from '../views/Main.vue'
+import CategoryEdit from '../views/CategoryEdit.vue'
+import CategoryList from '../views/CategoryList.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    name: 'main',
+    component: Main,
+    children: [
+      {
+        path: '/categories/create',
+        component: CategoryEdit
+      },
+      {
+        path: '/categories/list',
+        component: CategoryList
+      }
+    ]
   }
 ]
 
