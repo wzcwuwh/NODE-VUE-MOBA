caa72e98c099eeedaf60e89d25cf54a537533378
wuzaichang <wuzaichang@wuzaichangdembp.cn.ibm.com>
2019-12-22 15:17:36 +0800

    Add category list UI and logic

--- admin/src/views/CategoryList.vue
+++ admin/src/views/CategoryList.vue
@@ -1,5 +1,30 @@
 <template>
   <div>
-    分类列表
+    <h1>分类列表</h1>
+    <el-table :data="items">
+        <el-table-column prop="_id" label="ID" width="240">
+        </el-table-column>
+        <el-table-column prop="name" label="姓名">
+        </el-table-column>
+      </el-table>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: []
+    }
+  },
+  methods: {
+    async fetch(){
+      const res = await this.$http.get('categories')
+      this.items = res.data
+    }
+  },
+  created() {
+    this.fetch()
+  },
+}
+</script>
